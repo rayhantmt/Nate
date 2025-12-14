945f6b93a62667d52b5b1f5da2db189abe3fdfae
rayhantmt <rayhantmt@gmail.com>
2025-12-15 03:34:23 +0600

    made the splash screen

--- lib/modules/splash/splash.dart
+++ lib/modules/splash/splash.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:nate/utils/app_images.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({super.key});
 
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
