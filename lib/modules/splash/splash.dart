f29730144f1f2b0d0b5d732e8d0950af68bbfb02
rayhantmt <rayhantmt@gmail.com>
2025-12-15 03:44:29 +0600

    added the future delayed

--- lib/modules/splash/splash.dart
+++ lib/modules/splash/splash.dart
@@ -6,15 +6,18 @@ class Splash extends StatefulWidget {
 
   @override
   State<Splash> createState() => _SplashState();
+  
 }
 
 class _SplashState extends State<Splash> {
   @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2));
+  
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Image.asset(AppImages.splash,
-      fit: BoxFit.cover,
-      ),
-    );
+    return Scaffold(body: Image.asset(AppImages.splash, fit: BoxFit.cover));
   }
 }
