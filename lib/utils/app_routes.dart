85cceaa6d20761be2dfacdf033dcc2fa541a1132
rayhantmt <rayhantmt@gmail.com>
2025-12-16 00:08:27 +0600

    added the binding in approutes

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,9 +1,10 @@
 import 'package:get/route_manager.dart';
+import 'package:nate/modules/login/log_in_binding.dart';
 import 'package:nate/modules/login/log_in_view.dart';
 import 'package:nate/utils/app_pages.dart';
 
 class AppRoutes {
   static final pages=[
-    GetPage(name: AppPages.login, page: () => LogInView(),)
+    GetPage(name: AppPages.login, page: () => LogInView(),binding: LogInBinding()),
   ];
 }
