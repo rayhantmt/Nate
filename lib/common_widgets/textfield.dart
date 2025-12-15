a97a05e14e4a2fd35b0399ab25291f88c8220bc6
rayhantmt <rayhantmt@gmail.com>
2025-12-16 03:35:27 +0600

    ceated the textfield common widget

--- lib/common_widgets/textfield.dart
+++ lib/common_widgets/textfield.dart
@@ -2,18 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Commontextfield extends StatelessWidget {
-  const Commontextfield({super.key});
+  const Commontextfield({super.key, required this.tittle, required this.hint});
+final String tittle;
+final String hint;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: Get.height*0.1,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.white.withOpacity(0.8)
-        )
-      ),
+    return Column(
+      children: [
+        Text(tittle),
+        Container(
+          height: Get.height*0.1,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.white.withOpacity(0.8)
+            )
+          ),
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hint: Text(hint)
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
