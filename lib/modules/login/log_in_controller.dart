d482bf1f101129893ad2da025dd0d4e30ff8d519
rayhantmt <rayhantmt@gmail.com>
2025-12-16 00:12:57 +0600

    created the controller

--- lib/modules/login/log_in_controller.dart
+++ lib/modules/login/log_in_controller.dart
@@ -1,3 +1,8 @@
 import 'package:get/get.dart';
 
-class LogInController extends GetxController{}
+class LogInController extends GetxController{
+  var logintype=0.obs;
+void setlogintype(int type){
+logintype.value=type;
+}
+}
