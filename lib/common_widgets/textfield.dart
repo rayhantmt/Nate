93d3efef07c146deaf5e91d99c400597760043ef
rayhantmt <rayhantmt@gmail.com>
2025-12-16 04:46:37 +0600

    styling the textforrm

--- lib/common_widgets/textfield.dart
+++ lib/common_widgets/textfield.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Commontextfield extends StatelessWidget {
   const Commontextfield({super.key, required this.tittle, required this.hint});
@@ -9,20 +10,32 @@ final String hint;
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(tittle),
+        Text(tittle,
+        style: GoogleFonts.roboto( fontWeight: FontWeight.w600,fontSize: 14,color: Colors.white),
+        ),
         Container(
-          height: Get.height*0.1,
+          height: Get.height*0.04,
           width: double.infinity,
           decoration: BoxDecoration(
             border: Border.all(
               color: Colors.white.withOpacity(0.8)
-            )
+            ),
+            borderRadius: BorderRadius.circular(12)
           ),
           child: TextFormField(
             decoration: InputDecoration(
               border: InputBorder.none,
-              hint: Text(hint)
+              hint: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(hint,
+                style: GoogleFonts.roboto( fontWeight: FontWeight.w400,fontSize: 12,
+                color: Colors.white.withOpacity(0.6)
+                ),
+                ),
+              )
             ),
           ),
         ),
