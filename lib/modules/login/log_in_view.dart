2ad0abdd7e03dc59eb37bcbeba4d818195bc13cb
rayhantmt <rayhantmt@gmail.com>
2025-12-16 00:23:03 +0600

    fomatted the code

--- lib/modules/login/log_in_view.dart
+++ lib/modules/login/log_in_view.dart
@@ -9,30 +9,27 @@ class LogInView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffADADAD),
-    body: SafeArea(
-      child: Column(
-        children: [
-          Image.asset(AppImages.login),
-          Expanded(child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15),
-                
-              )
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset(AppImages.login),
+            Expanded(
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                ),
+                child: Column(children: [SizedBox(height: Get.height * 0.1)]),
+              ),
             ),
-            child: Column(
-              children: [
-SizedBox(height: Get.height*0.1,)
-              ],
-            ),
-          ))
-        ],
+          ],
+        ),
       ),
-    ),
     );
   }
 }
