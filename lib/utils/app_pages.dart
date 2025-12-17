9a11fd7c1f2c99228f57f86323c346b045c72f3b
rayhantmt <rayhantmt@gmail.com>
2025-12-18 04:16:41 +0600

    adding the reset pass page

--- lib/utils/app_pages.dart
+++ lib/utils/app_pages.dart
@@ -2,4 +2,5 @@ class AppPages {
   static const String login='/login';
   static const String forgotpassword='/forgotpassword';
   static const String otppage='/otppage';
+  static const String resetpass='/resetpass';
 }
