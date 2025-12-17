550eaf7e8786dfc1d422915cc47900bbf1e57b0a
rayhantmt <rayhantmt@gmail.com>
2025-12-17 22:11:42 +0600

    added the app bar

--- lib/modules/forgot_password/forgor_password_view.dart
+++ lib/modules/forgot_password/forgor_password_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/utils/app_images.dart';
 
 class ForgorPasswordView extends StatelessWidget {
@@ -17,9 +18,20 @@ class ForgorPasswordView extends StatelessWidget {
               children: [
                 Row(
               children: [
-                Image.asset(AppImages.backicon,
-                height:Get.height*0.1,
-                width: Get.width*0.15,
+                GestureDetector(
+                  onTap: () => Get.back(),
+                  child: Image.asset(AppImages.backicon,
+                  height:Get.height*0.1,
+                  width: Get.width*0.15,
+                  ),
+                ),
+                SizedBox(width: Get.width*0.05,),
+                Text('Forgot Password',
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                  color: Colors.white
+                ),
                 )
               ],
             
