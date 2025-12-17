948b15455198ddb7b23278600c61ada2a68bd7a4
rayhantmt <rayhantmt@gmail.com>
2025-12-18 03:37:51 +0600

    added the otp page in app pages

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -2,11 +2,13 @@ import 'package:get/route_manager.dart';
 import 'package:nate/modules/forgot_password/forgor_password_view.dart';
 import 'package:nate/modules/login/log_in_binding.dart';
 import 'package:nate/modules/login/log_in_view.dart';
+import 'package:nate/modules/otp_page/otp_page.dart';
 import 'package:nate/utils/app_pages.dart';
 
 class AppRoutes {
   static final pages=[
     GetPage(name: AppPages.login, page: () => LogInView(),binding: LogInBinding()),
-    GetPage(name: AppPages.forgotpassword, page: () => ForgorPasswordView(),)
+    GetPage(name: AppPages.forgotpassword, page: () => ForgorPasswordView(),),
+    GetPage(name: AppPages.otppage, page: () => OtpPageView(),)
   ];
 }
