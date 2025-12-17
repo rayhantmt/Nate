127e085f1b71abc70c32aa01e8a0b2a15b04b4bb
rayhantmt <rayhantmt@gmail.com>
2025-12-18 03:41:35 +0600

    added the app bar

--- lib/modules/otp_page/otp_page.dart
+++ lib/modules/otp_page/otp_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nate/utils/app_images.dart';
 
 class OtpPageView extends StatelessWidget {
   const OtpPageView({super.key});
@@ -7,6 +10,28 @@ class OtpPageView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20,right: 20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(AppImages.backicon),
+                  SizedBox(width: Get.width*0.03,),
+                  Text('Verify your email',
+                  style: GoogleFonts.roboto( 
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    color: Colors.white
+                  ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
