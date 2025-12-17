b52b494a1a5153166755f29872a94b9aba378987
rayhantmt <rayhantmt@gmail.com>
2025-12-17 21:13:09 +0600

    added the forgot password in app pages

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:get/route_manager.dart';
+import 'package:nate/modules/forgot_password/forgor_password_view.dart';
 import 'package:nate/modules/login/log_in_binding.dart';
 import 'package:nate/modules/login/log_in_view.dart';
 import 'package:nate/utils/app_pages.dart';
@@ -6,5 +7,6 @@ import 'package:nate/utils/app_pages.dart';
 class AppRoutes {
   static final pages=[
     GetPage(name: AppPages.login, page: () => LogInView(),binding: LogInBinding()),
+    GetPage(name: AppPages.forgotpassword, page: () => ForgorPasswordView(),)
   ];
 }
