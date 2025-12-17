a7b334d406db3e8c2ef0744647027c7ea106d0db
rayhantmt <rayhantmt@gmail.com>
2025-12-17 22:27:39 +0600

    done with designing the button

--- lib/common_widgets/common_button.dart
+++ lib/common_widgets/common_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CommonButton extends StatelessWidget {
   const CommonButton({super.key, required this.tittle});
@@ -7,9 +8,20 @@ final String tittle;
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: Get.height*0.1,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Color(0xff2E3030)
+      ),
+      height: Get.height*0.05,
       width: double.infinity,
-      child: Text(tittle),
+      child: Center(child: Text(tittle,
+      style: GoogleFonts.roboto( 
+        color: Color(0xffEC1B34),
+        fontWeight: FontWeight.w600,
+        fontSize: 16,
+      
+      ),
+      )),
     );
   }
 }
