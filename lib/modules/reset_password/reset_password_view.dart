be736cf0983dd4d0758389c62a91b4c8b8e6c92b
rayhantmt <rayhantmt@gmail.com>
2025-12-18 05:03:57 +0600

    desinging the reset pass

--- lib/modules/reset_password/reset_password_view.dart
+++ lib/modules/reset_password/reset_password_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:nate/utils/app_images.dart';
 
 class ResetPasswordView extends StatelessWidget {
   const ResetPasswordView({super.key});
@@ -7,6 +9,20 @@ class ResetPasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body:  SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(AppImages.backicon,
+                height:Get.height*0.1,
+                    width: Get.width*0.15,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
