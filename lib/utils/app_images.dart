60b77c88d448bf0715f18375f737e83fc1ab256b
rayhantmt <rayhantmt@gmail.com>
2026-01-02 02:21:00 +0600

    added the images in image class

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -3,4 +3,6 @@ class AppImages {
   static const String login='assets/log_in.png';
   static const String backicon='assets/back_icon.png';
   static const String homebackground='assets/home_background.png';
+  static const String settingsicon='assets/setting_icon.png';
+  static const String profileiamge='assets/profile_frame.png';
 }
