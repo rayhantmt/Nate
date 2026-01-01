24ab0a712f32d5616c09698a621989f5e7d41f6a
rayhantmt <rayhantmt@gmail.com>
2026-01-02 04:06:46 +0600

    making settings screen

---
+++ lib/modules/settings/setting_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SettingView extends StatelessWidget {
+  const SettingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
