8c188cc5308184b8d805828a01a0915115acd5f5
rayhantmt <rayhantmt@gmail.com>
2026-01-02 02:47:07 +0600

    added the profile text and

--- lib/modules/profile/profile_view.dart
+++ lib/modules/profile/profile_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/utils/app_images.dart';
 
 class ProfileView extends StatelessWidget {
@@ -7,11 +9,40 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [Image.asset(AppImages.profileiamge,
-      fit: BoxFit.cover,
-      height: double.infinity,
-      width: double.infinity,
-      )]),
+      body: Stack(
+        children: [
+          Image.asset(
+            AppImages.profileiamge,
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Padding(
+            padding:EdgeInsets.only(left: 20,right: 20),
+            child: Column(
+              children: [
+                SizedBox(height: Get.height*0.05,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Profile',
+                    style:GoogleFonts.roboto( 
+                      fontWeight: FontWeight.w700,
+                      fontSize: 32,
+                      color: Colors.white
+                    ) ,
+                    ),
+                    Image.asset(AppImages.settingsicon,
+                    height: Get.height*0.05,
+                    width: Get.width*0.1,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
