40f0ca3ef5ba036c79ec672d58f385d7af0afaf3
rayhantmt <rayhantmt@gmail.com>
2026-01-02 03:14:43 +0600

    added the iamge

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -5,4 +5,5 @@ class AppImages {
   static const String homebackground='assets/home_background.png';
   static const String settingsicon='assets/setting_icon.png';
   static const String profileiamge='assets/profile_frame.png';
+  static const String editprofile='assets/edit_profile.png';
 }
