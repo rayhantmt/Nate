ebe21640e1141a41c1f42223f8fbaefa4376fec5
rayhantmt <rayhantmt@gmail.com>
2026-01-02 01:21:17 +0600

    added the email verify page

---
+++ lib/modules/email_verify_page/email_verify_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nate/common_widgets/common_button.dart';
+import 'package:nate/modules/email_verify_page/email_verify_controller.dart';
+import 'package:nate/utils/app_images.dart';
+import 'package:nate/utils/app_pages.dart';
+
+class EmailVerifyPage extends GetView<EmailVerifyController> {
+  const EmailVerifyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () => Get.back(),
+                      child: Image.asset(
+                        AppImages.backicon,
+                        height: Get.height * 0.1,
+                        width: Get.width * 0.15,
+                      ),
+                    ),
+
+                    SizedBox(width: Get.width * 0.03),
+                    Text(
+                      'Verify your email',
+                      style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+
+                Text(
+                  'Enter OTP',
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'We just sent you a 6 digit code to your email.  Please confirm it below.',
+                  maxLines: 3,
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(6, (index) {
+                    return SizedBox(
+                      width: 40,
+                      child: TextField(
+                        controller: controller.otpControllers[index],
+                        focusNode: controller.otpFocusNodes[index],
+                        keyboardType: TextInputType.number,
+                        maxLength: 1,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                        decoration: const InputDecoration(
+                          counterText: '',
+                          border: UnderlineInputBorder(),
+                        ),
+                        onChanged: (value) {
+                          controller.onOtpFieldChanged(value, index);
+                        },
+                      ),
+                    );
+                  }),
+                ),
+                const SizedBox(height: 16),
+                Obx(
+                  () => GestureDetector(
+                    onTap: controller.secondsRemaining.value == 0
+                        ? controller.resendCode
+                        : null,
+                    child: Text(
+                      controller.secondsRemaining.value == 0
+                          ? "Resend code"
+                          : "Resend code ${controller.secondsRemaining}s",
+                      style: TextStyle(
+                        color: controller.secondsRemaining.value == 0
+                            ? Colors.white
+                            : Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => Get.toNamed(AppPages.resetpass),
+                  child: CommonButton(tittle: 'Verify'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
