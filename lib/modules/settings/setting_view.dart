31085034d550af1c965ced85343c5ef6867564b1
rayhantmt <rayhantmt@gmail.com>
2026-01-02 04:23:20 +0600

    added the oidin dichilam

--- lib/modules/settings/setting_view.dart
+++ lib/modules/settings/setting_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nate/common_widgets/profile_container.dart';
+import 'package:nate/utils/app_images.dart';
 
 class SettingView extends StatelessWidget {
   const SettingView({super.key});
@@ -6,10 +8,14 @@ class SettingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Column(
+          children: [
+
+            ProfileContainer(tittle: 'Change Password', image: AppImages.changepass)
+          ],
+        ),
       ),
     );
   }
