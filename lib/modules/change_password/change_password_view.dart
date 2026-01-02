040e5ea525b42a72371ef32eea847db89d91e327
rayhantmt <rayhantmt@gmail.com>
2026-01-03 00:42:19 +0600

    added the first field with obsecuring

--- lib/modules/change_password/change_password_view.dart
+++ lib/modules/change_password/change_password_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/state_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nate/common_widgets/textfield.dart';
 import 'package:nate/modules/change_password/change_password_controller.dart';
 import 'package:nate/utils/app_images.dart';
 
@@ -17,23 +18,38 @@ class ChangePasswordView extends GetView<ChangePasswordController> {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              SizedBox(height: Get.height*0.06,),
+              SizedBox(height: Get.height * 0.06),
               Row(
                 children: [
-                  Image.asset(AppImages.backicon,
-                  height: Get.height * 0.05,
-                      width: Get.width * 0.1,
+                  Image.asset(
+                    AppImages.backicon,
+                    height: Get.height * 0.05,
+                    width: Get.width * 0.1,
                   ),
-                  SizedBox(width: Get.width*0.07,),
-                  Text('Change Password',
-                  style: GoogleFonts.openSans( 
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20,
-                    color: Colors.white
-                  ),
+                  SizedBox(width: Get.width * 0.07),
+                  Text(
+                    'Change Password',
+                    style: GoogleFonts.openSans(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
                   ),
                 ],
-              )
+              ),
+              Obx(() => Commontextfield(
+                tittle: 'Current Password',
+                hint: '*********',
+                obsecuretext: controller.obsecuretext.value,
+                suffix: GestureDetector(
+                  onTap: () => controller.togglepass(),
+                  child: Icon(
+                    controller.obsecuretext.value
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                ),
+              ),)
             ],
           ),
         ),
