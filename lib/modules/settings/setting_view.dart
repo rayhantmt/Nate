b60327313b1b0f7e76c483827a105f2a2b84104f
rayhantmt <rayhantmt@gmail.com>
2026-01-02 22:18:34 +0600

    added back navigation

--- lib/modules/settings/setting_view.dart
+++ lib/modules/settings/setting_view.dart
@@ -15,43 +15,56 @@ class SettingView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            SizedBox(height: Get.height*0.1),
-            Row(children: [
-              Image.asset(AppImages.backicon,
-              height: Get.height * 0.05,
-                        width: Get.width * 0.1,
-              )
-            ],),
+            SizedBox(height: Get.height * 0.1),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () => Get.back(),
+                  child: Image.asset(
+                    AppImages.backicon,
+                    height: Get.height * 0.05,
+                    width: Get.width * 0.1,
+                  ),
+                ),
+              ],
+            ),
             ProfileContainer(
               tittle: 'Change Password',
               image: AppImages.changepass,
             ),
-            SizedBox(height: Get.height*0.03,),
+            SizedBox(height: Get.height * 0.03),
             Container(
-      height: Get.height * 0.05,
-      width: Get.width * 0.9,
-      decoration: BoxDecoration(
-        color: Color(0xffFFFFFF).withOpacity(0.1),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        children: [
-          Image.asset(AppImages.deleteacc, height: Get.height * 0.03, width: Get.width * 0.1),
-          Text('Delete account',
-          style:GoogleFonts.roboto( 
-            fontWeight: FontWeight.w500,
-            fontSize: 14,
-            color: Color(0xffEC1B34)
-          ) ,
-          ),
-          Expanded(child: SizedBox()),
-          Image.asset(AppImages.editprofileback,
-          color: Color(0xffEC1B34),
-          height: Get.height * 0.03, width: Get.width * 0.1
-          )
-        ],
-      ),
-    ),
+              height: Get.height * 0.05,
+              width: Get.width * 0.9,
+              decoration: BoxDecoration(
+                color: Color(0xffFFFFFF).withOpacity(0.1),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                children: [
+                  Image.asset(
+                    AppImages.deleteacc,
+                    height: Get.height * 0.03,
+                    width: Get.width * 0.1,
+                  ),
+                  Text(
+                    'Delete account',
+                    style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      color: Color(0xffEC1B34),
+                    ),
+                  ),
+                  Expanded(child: SizedBox()),
+                  Image.asset(
+                    AppImages.editprofileback,
+                    color: Color(0xffEC1B34),
+                    height: Get.height * 0.03,
+                    width: Get.width * 0.1,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
