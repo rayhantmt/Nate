a08dc1e666997c79ad75c8805394a7f91e824cf2
rayhantmt <rayhantmt@gmail.com>
2026-01-02 23:00:11 +0600

    done with the  edit profile screen

--- lib/modules/edit_profile/edit_profile_view.dart
+++ lib/modules/edit_profile/edit_profile_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/state_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nate/common_widgets/common_button.dart';
+import 'package:nate/common_widgets/textfield.dart';
 import 'package:nate/modules/edit_profile/edit_profile_controller.dart';
 import 'package:nate/utils/app_images.dart';
 
@@ -36,7 +38,7 @@ class EditProfileView extends GetView<EditProfileController> {
                   ),
                 ],
               ),
-              SizedBox(height: Get.height * 0.05),
+              SizedBox(height: Get.height * 0.03),
               CircleAvatar(
                 radius: Get.height * 0.07,
                 child: ClipOval(
@@ -48,6 +50,15 @@ class EditProfileView extends GetView<EditProfileController> {
                   ),
                 ),
               ),
+                SizedBox(height: Get.height * 0.03),
+                Commontextfield(tittle: 'First Name', hint: 'sunan', obsecuretext: false),
+                  SizedBox(height: Get.height * 0.03),
+                Commontextfield(tittle: 'Last Name', hint: 'sunan', obsecuretext: false),
+                  SizedBox(height: Get.height * 0.03),
+                Commontextfield(tittle: 'Email Address', hint: 'something@gmail.com', obsecuretext: false),  SizedBox(height: Get.height * 0.03),
+                Commontextfield(tittle: 'Contact Number', hint: '(719) 266-2837', obsecuretext: false),
+                SizedBox(height: Get.height * 0.03),
+                CommonButton(tittle: 'Save Changes')
             ],
           ),
         ),
