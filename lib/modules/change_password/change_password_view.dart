ed95341ab2dc4255ce2fc147b5ef28687570e253
rayhantmt <rayhantmt@gmail.com>
2026-01-03 00:14:43 +0600

    started working on change password

---
+++ lib/modules/change_password/change_password_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChangePasswordView extends StatelessWidget {
+  const ChangePasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff17191A),
+    );
+  }
+}
