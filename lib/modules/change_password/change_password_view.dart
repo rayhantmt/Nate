7a3062ca8269456217055ded1752450d51ab89d2
rayhantmt <rayhantmt@gmail.com>
2026-01-03 00:30:45 +0600

    added the app bar of change pass

--- lib/modules/change_password/change_password_view.dart
+++ lib/modules/change_password/change_password_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/state_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/modules/change_password/change_password_controller.dart';
+import 'package:nate/utils/app_images.dart';
 
 class ChangePasswordView extends GetView<ChangePasswordController> {
   const ChangePasswordView({super.key});
@@ -10,10 +13,29 @@ class ChangePasswordView extends GetView<ChangePasswordController> {
     return Scaffold(
       backgroundColor: Color(0xff17191A),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text('Change Password')
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: Get.height*0.06,),
+              Row(
+                children: [
+                  Image.asset(AppImages.backicon,
+                  height: Get.height * 0.05,
+                      width: Get.width * 0.1,
+                  ),
+                  SizedBox(width: Get.width*0.07,),
+                  Text('Change Password',
+                  style: GoogleFonts.openSans( 
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    color: Colors.white
+                  ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
