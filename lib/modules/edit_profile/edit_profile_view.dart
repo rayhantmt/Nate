c6402185b3a407d9a21c4674ba405692034d962b
rayhantmt <rayhantmt@gmail.com>
2026-01-02 22:50:04 +0600

    working on edit profile screen

--- lib/modules/edit_profile/edit_profile_view.dart
+++ lib/modules/edit_profile/edit_profile_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/state_manager.dart';
 import 'package:nate/modules/edit_profile/edit_profile_controller.dart';
+import 'package:nate/utils/app_images.dart';
 
 class EditProfileView extends GetView<EditProfileController> {
   const EditProfileView({super.key});
@@ -10,10 +12,21 @@ class EditProfileView extends GetView<EditProfileController> {
     return Scaffold(
       backgroundColor: Color(0xff17191A),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text('Edit Profie')
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: Get.height*0.07,),
+              Row(
+                children: [
+                  Image.asset(AppImages.backicon,
+                     height: Get.height * 0.05,
+                      width: Get.width * 0.1,
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
