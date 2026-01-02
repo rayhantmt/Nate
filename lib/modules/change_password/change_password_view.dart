73b64aad72717da685b0dfcccc48c486188a9791
rayhantmt <rayhantmt@gmail.com>
2026-01-03 00:54:57 +0600

    done with the change password screen

--- lib/modules/change_password/change_password_view.dart
+++ lib/modules/change_password/change_password_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/state_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nate/common_widgets/common_button.dart';
 import 'package:nate/common_widgets/textfield.dart';
 import 'package:nate/modules/change_password/change_password_controller.dart';
 import 'package:nate/utils/app_images.dart';
@@ -17,6 +18,7 @@ class ChangePasswordView extends GetView<ChangePasswordController> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: Get.height * 0.06),
               Row(
@@ -37,19 +39,64 @@ class ChangePasswordView extends GetView<ChangePasswordController> {
                   ),
                 ],
               ),
-              Obx(() => Commontextfield(
-                tittle: 'Current Password',
-                hint: '*********',
-                obsecuretext: controller.obsecuretext.value,
-                suffix: GestureDetector(
-                  onTap: () => controller.togglepass(),
-                  child: Icon(
-                    controller.obsecuretext.value
-                        ? Icons.visibility
-                        : Icons.visibility_off,
+              SizedBox(height: Get.height * 0.02),
+              Obx(
+                () => Commontextfield(
+                  tittle: 'Current Password',
+                  hint: '*********',
+                  obsecuretext: controller.obsecuretext.value,
+                  suffix: GestureDetector(
+                    onTap: () => controller.togglepass(),
+                    child: Icon(
+                      controller.obsecuretext.value
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
                   ),
                 ),
-              ),)
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Obx(
+                () => Commontextfield(
+                  tittle: 'New Password',
+                  hint: '*********',
+                  obsecuretext: controller.obsecuretext.value,
+                  suffix: GestureDetector(
+                    onTap: () => controller.togglepass(),
+                    child: Icon(
+                      controller.obsecuretext.value
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Obx(
+                () => Commontextfield(
+                  tittle: 'Confirm New Password',
+                  hint: '*********',
+                  obsecuretext: controller.obsecuretext1.value,
+                  suffix: GestureDetector(
+                    onTap: () => controller.togglepass1(),
+                    child: Icon(
+                      controller.obsecuretext1.value
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.02),
+            Text('Forgot the password?',
+            style: GoogleFonts.roboto( 
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+              color: Color(0xffEC1B34)
+            ),
+            ),
+            SizedBox(height: Get.height * 0.02),
+            CommonButton(tittle: 'Update Password')
             ],
           ),
         ),
