70331f113d85afd0f4f5474fda27db54fa2fdd77
rayhantmt <rayhantmt@gmail.com>
2026-01-31 01:42:45 +0600

    formatted the document

--- lib/modules/new_proposal/new_proposal_binding.dart
+++ lib/modules/new_proposal/new_proposal_binding.dart
@@ -1,10 +1,9 @@
 import 'package:get/get.dart';
-import 'package:get/get_instance/src/bindings_interface.dart';
 import 'package:nate/modules/new_proposal/new_proposal_controller.dart';
 
-class NewProposalBinding extends Bindings{
+class NewProposalBinding extends Bindings {
   @override
   void dependencies() {
-   Get.lazyPut<NewProposalController>(() => NewProposalController(),);
+    Get.lazyPut<NewProposalController>(() => NewProposalController());
   }
 }
