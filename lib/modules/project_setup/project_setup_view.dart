c4d3fb1c21abb4c3c4b3f98836d8fcda254e8ffc
rayhantmt <rayhantmt@gmail.com>
2026-01-31 03:43:44 +0600

    showing the captured image in the view

--- lib/modules/project_setup/project_setup_view.dart
+++ lib/modules/project_setup/project_setup_view.dart
@@ -6,8 +6,8 @@ import 'package:nate/modules/new_proposal/new_proposal_controller.dart';
 import 'package:nate/utils/app_images.dart';
 
 class ProjectSetupView extends StatelessWidget {
-   ProjectSetupView({super.key});
-final controller=Get.find<NewProposalController>();
+  ProjectSetupView({super.key});
+  final controller = Get.find<NewProposalController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,27 +17,27 @@ final controller=Get.find<NewProposalController>();
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              SizedBox(
-                height: Get.height*0.07,
-              ),
+              SizedBox(height: Get.height * 0.07),
               Row(
                 children: [
                   GestureDetector(
                     onTap: () => Get.back(),
-                    child: Image.asset(AppImages.backicon,
-                    height: Get.height*0.04,
+                    child: Image.asset(
+                      AppImages.backicon,
+                      height: Get.height * 0.04,
                     ),
                   ),
                 ],
               ),
               ClipRRect(
                 borderRadius: BorderRadiusGeometry.circular(12),
-                child: Image.file(File(controller.pickedimage.value!.path),
-                height: Get.height*0.4,
-                width: double.infinity,
-                fit: BoxFit.cover,
+                child: Image.file(
+                  File(controller.pickedimage.value!.path),
+                  height: Get.height * 0.4,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
                 ),
-              )
+              ),
             ],
           ),
         ),
