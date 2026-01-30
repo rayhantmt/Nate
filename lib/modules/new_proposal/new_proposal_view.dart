a40f848b0beca0f72f4d64a7b3801266359a9a3b
rayhantmt <rayhantmt@gmail.com>
2026-01-31 01:57:00 +0600

    added create new page tex

--- lib/modules/new_proposal/new_proposal_view.dart
+++ lib/modules/new_proposal/new_proposal_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:nate/utils/app_images.dart';
 
 class NewProposalView extends StatelessWidget {
@@ -8,10 +9,30 @@ class NewProposalView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
-      body: Column(
-        children: [
-          Text('New prop')
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(width:double.infinity,),
+           Column(
+             children: [
+               Text('Create new project',
+               style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 22,
+                 color: Color(0xffEC1B34),
+               ),
+               ),
+               Icon(Icons.add,
+               color: Color(0xffEC1B34),
+               size: 45,
+               )
+             ],
+           )
+          ],
+        ),
       ),
     );
   }
