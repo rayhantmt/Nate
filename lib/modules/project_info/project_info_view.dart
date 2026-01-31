101762e60180ddcce2cd6f120ef10f8cd6df4acb
rayhantmt <rayhantmt@gmail.com>
2026-02-01 05:04:39 +0600

    added the background color

--- lib/modules/project_info/project_info_view.dart
+++ lib/modules/project_info/project_info_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nate/utils/app_images.dart';
 
 class ProjectInfoView extends StatelessWidget {
   const ProjectInfoView({super.key});
@@ -6,11 +7,12 @@ class ProjectInfoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppImages.primarycolor,
       body: SingleChildScrollView(
         child: Column(
           children: [
             Row(children: [
-              
+
             ],)
           ],
         ),
