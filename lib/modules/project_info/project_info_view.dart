46f5e6c02b92510560490e6304536c523d6ad042
rayhantmt <rayhantmt@gmail.com>
2026-02-03 04:23:12 +0600

    added allignment

--- lib/modules/project_info/project_info_view.dart
+++ lib/modules/project_info/project_info_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/utils/app_images.dart';
 
 class ProjectInfoView extends StatelessWidget {
@@ -13,12 +14,36 @@ class ProjectInfoView extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(
+                height: Get.height*0.06,
+              ),
               Row(children: [
-              Image.asset(AppImages.backicon,
-              height: Get.height*0.07,
+              GestureDetector(
+                onTap: () => Get.back(),
+                child: Image.asset(AppImages.backicon,
+                height: Get.height*0.07,
+                ),
+              ),
+              SizedBox(width: Get.width*0.05,),
+              Text('Project Info',
+              style: GoogleFonts.openSans( 
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+                color: Colors.white
+              ),
               )
-              ],)
+              ],),
+              SizedBox(height: Get.height*0.02,),
+              Text('Shipping Address',
+              style: GoogleFonts.roboto( 
+                fontWeight: FontWeight.w600,
+                fontSize: 14,
+                color: Colors.white
+              ),
+              )
+
             ],
           ),
         ),
