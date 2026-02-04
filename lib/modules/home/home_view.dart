9f29a1ab31daf4a5fba08cf728f306fa8d0a89ac
rayhantmt <rayhantmt@gmail.com>
2026-02-05 03:17:43 +0600

    added the home controller and data models

--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -1,42 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nate/modules/home/home_controller.dart';
 import 'package:nate/utils/app_images.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Image.asset(AppImages.mainhomebackground,
-        height: double.infinity,
-        width: double.infinity,
-        fit: BoxFit.cover,
+        Image.asset(
+          AppImages.mainhomebackground,
+          height: double.infinity,
+          width: double.infinity,
+          fit: BoxFit.cover,
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              SizedBox(height: Get.height*0.05,),
+              SizedBox(height: Get.height * 0.05),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Image.asset(AppImages.homelogo,
-                  height: Get.height*0.15,
-                  width: Get.width*0.30,
+                  Image.asset(
+                    AppImages.homelogo,
+                    height: Get.height * 0.15,
+                    width: Get.width * 0.30,
                   ),
                   CircleAvatar(
                     backgroundImage: AssetImage(AppImages.profileimg),
-                  )
+                  ),
                 ],
               ),
-              
+
               Image.asset(AppImages.colourfuldivider),
-              SizedBox(height: Get.height*0.02,),
-               Container(
-                height: Get.height*0.06,
+              SizedBox(height: Get.height * 0.02),
+              Container(
+                height: Get.height * 0.06,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(14),
@@ -48,24 +51,23 @@ class HomeView extends StatelessWidget {
                     contentPadding: EdgeInsets.only(left: 10),
                     hint: Row(
                       children: [
-                        Icon(Icons.search,
-                        color: Colors.white,
-                        ),
-                        Text('Search for project',
-                        style: GoogleFonts.roboto( 
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14,
-                          color: Colors.white.withOpacity(0.7)
-                        ),
+                        Icon(Icons.search, color: Colors.white),
+                        Text(
+                          'Search for project',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: Colors.white.withOpacity(0.7),
+                          ),
                         ),
                       ],
-                    )
+                    ),
                   ),
                 ),
-               )
+              ),
             ],
           ),
-        )
+        ),
       ],
     );
   }
