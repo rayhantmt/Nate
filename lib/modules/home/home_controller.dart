c216c201008ab12d6af7845ff8d319b93449e642
rayhantmt <rayhantmt@gmail.com>
2026-02-05 03:15:47 +0600

    added the data models

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -2,8 +2,42 @@ import 'package:get/state_manager.dart';
 import 'package:nate/modules/home/home_model.dart';
 
 class HomeController extends GetxController {
-  var homedata=<HomeModel>[
-    HomeModel(address: '120 Baker Street, Los Angeles, CA 90012', price: 185, vents: 3, progress: true)
+  var homedata = <HomeModel>[
+    HomeModel(
+      address: '120 Baker Street, Los Angeles, CA 90012',
+      price: 185,
+      vents: 3,
+      progress: true,
+    ),
+    HomeModel(
+      address: '120 Baker Street, Los Angeles, CA 90012',
+      price: 111,
+      vents: 5,
+      progress: true,
+    ),
+    HomeModel(
+      address: '120 Baker Street, Los Angeles, CA 90012',
+      price: 185,
+      vents: 3,
+      progress: true,
+    ),
+    HomeModel(
+      address: '120 Baker Street, Los Angeles, CA 90012',
+      price: 111,
+      vents: 5,
+      progress: true,
+    ),
+    HomeModel(
+      address: '120 Baker Street, Los Angeles, CA 90012',
+      price: 185,
+      vents: 3,
+      progress: true,
+    ),
+    HomeModel(
+      address: '120 Baker Street, Los Angeles, CA 90012',
+      price: 111,
+      vents: 5,
+      progress: true,
+    ),
   ].obs;
-  
 }
---
+++ lib/modules/home/home_model.dart
@@ -0,0 +1,9 @@
+class HomeModel {
+  final String address;
+  final double price;
+  final double vents;
+  final bool progress;
+
+  HomeModel({required this.address, required this.price, required this.vents, required this.progress});
+  
+}
