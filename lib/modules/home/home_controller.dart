2df017b415039fb6eaa86165c99f92d0066a2f4d
rayhantmt <rayhantmt@gmail.com>
2026-02-05 03:14:31 +0600

    adding the hoem data

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -1,3 +1,9 @@
 import 'package:get/state_manager.dart';
+import 'package:nate/modules/home/home_model.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  var homedata=<HomeModel>[
+    HomeModel(address: '120 Baker Street, Los Angeles, CA 90012', price: 185, vents: 3, progress: true)
+  ].obs;
+  
+}
