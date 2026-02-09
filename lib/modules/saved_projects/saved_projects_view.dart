4a0038f1da5c1f9b93e85bcd7be8c39d817b5dfc
rayhantmt <rayhantmt@gmail.com>
2026-02-10 02:16:31 +0600

    done with formatting also

--- lib/modules/saved_projects/saved_projects_view.dart
+++ lib/modules/saved_projects/saved_projects_view.dart
@@ -10,134 +10,127 @@ class SavedProjectsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final scontorller=Get.find<HomeController>();
+    final scontorller = Get.find<HomeController>();
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            SizedBox(
-              height: Get.height*0.07,
-            ),
+            SizedBox(height: Get.height * 0.07),
             Row(
               children: [
-                Image.asset(AppImages.backicon,
-                height: Get.height*0.06,
-                ),
-                SizedBox(
-                  width: Get.width*0.05,
-                ),
-                Text('Saved Projects',
-                style: GoogleFonts.openSans( 
-                  fontWeight: FontWeight.w700,
-                  fontSize: 20,
-                  color: Colors.white
-                ),
-                )
-              ],
-            ),
-                 Expanded(
-                  child: ListView.builder(
-                    itemCount: scontorller.homedata.length,
-        
-                    itemBuilder: (context, index) => Column(
-                      children: [
-                        GestureDetector(
-                          onTap: () => Get.toNamed(AppPages.hoeproj4ectdetails),
-                          child: Container(
-                            height: Get.height * 0.17,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(18),
-                              color: Colors.white.withOpacity(0.05),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 20,
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                        child: Text(
-                                          scontorller.homedata[index].address,
-                                          style: GoogleFonts.openSans(
-                                            fontWeight: FontWeight.w700,
-                                            fontSize: 20,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        height: Get.height * 0.04,
-                                        width: Get.width * 0.32,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(17),
-                                          color: Color(
-                                            0xffF97400,
-                                          ).withOpacity(0.1),
-                                        ),
-                                        child: Row(
-                                          children: [
-                                            Icon(
-                                              Icons.circle,
-                                              color: Color(0xffF97400),
-                                              size: 10,
-                                            ),
-                                            Text(
-                                              scontorller.homedata[index].progress
-                                                  ? 'In Progress'
-                                                  : 'Delivered',
-                                              style: GoogleFonts.robotoMono(
-                                                color: Color(0xffF97400),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      SizedBox(width: Get.width * 0.01),
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 20,
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        'Estimate : ${scontorller.homedata[index].price.toString()}',
-                                        style: GoogleFonts.roboto(
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 14,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                      Text(
-                                        'Vents : ${scontorller.homedata[index].vents.toString()}',
-                                        style: GoogleFonts.roboto(
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 14,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: Get.height * 0.015),
-                      ],
-                    ),
+                Image.asset(AppImages.backicon, height: Get.height * 0.06),
+                SizedBox(width: Get.width * 0.05),
+                Text(
+                  'Saved Projects',
+                  style: GoogleFonts.openSans(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: scontorller.homedata.length,
+
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () => Get.toNamed(AppPages.hoeproj4ectdetails),
+                      child: Container(
+                        height: Get.height * 0.17,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18),
+                          color: Colors.white.withOpacity(0.05),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      scontorller.homedata[index].address,
+                                      style: GoogleFonts.openSans(
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: Get.height * 0.04,
+                                    width: Get.width * 0.32,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(17),
+                                      color: Color(0xffF97400).withOpacity(0.1),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.circle,
+                                          color: Color(0xffF97400),
+                                          size: 10,
+                                        ),
+                                        Text(
+                                          scontorller.homedata[index].progress
+                                              ? 'In Progress'
+                                              : 'Delivered',
+                                          style: GoogleFonts.robotoMono(
+                                            color: Color(0xffF97400),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(width: Get.width * 0.01),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'Estimate : ${scontorller.homedata[index].price.toString()}',
+                                    style: GoogleFonts.roboto(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Vents : ${scontorller.homedata[index].vents.toString()}',
+                                    style: GoogleFonts.roboto(
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: Get.height * 0.015),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
