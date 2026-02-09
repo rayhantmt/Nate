89bdd94061513db06b33ce030c17c8ab340a84f4
rayhantmt <rayhantmt@gmail.com>
2026-02-09 22:48:03 +0600

    added the quote generation section

---
+++ lib/modules/quote_generation/quote_generation_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nate/utils/app_images.dart';
+
+class QuoteGenerationView extends StatelessWidget {
+  const QuoteGenerationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset(AppImages.homelogo)
+        ],
+      ),
+    );
+  }
+}
