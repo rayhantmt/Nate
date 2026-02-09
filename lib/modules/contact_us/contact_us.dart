27d2cb42708b140c9acce8df2e851d8a6f5b3189
rayhantmt <rayhantmt@gmail.com>
2026-02-10 02:43:07 +0600

    done with the contact us section

--- lib/modules/contact_us/contact_us.dart
+++ lib/modules/contact_us/contact_us.dart
@@ -20,8 +20,11 @@ class ContactUs extends StatelessWidget {
               SizedBox(height: Get.height*0.06,),
               Row(
                 children: [
-                  Image.asset(AppImages.backicon,
-                  height: Get.height*0.06,
+                  GestureDetector(
+                    onTap: () => Get.back(),
+                    child: Image.asset(AppImages.backicon,
+                    height: Get.height*0.06,
+                    ),
                   ),
                   SizedBox(width: Get.width*0.05,),
                   Text('Contact Us',
@@ -44,6 +47,16 @@ class ContactUs extends StatelessWidget {
 
               Text('Lorem ipsum dolor sit amet consectetur. Ultrices id feugiat venenatis habitant mattis viverra elementum purus volutpat. Lacus eu molestie pulvinar rhoncus integer proin elementum. Pretium sit fringilla massa tristique aenean commodo leo. Aliquet viverra amet sit porta elementum et pellentesque posuere. Ullamcorper viverra tortor lobortis viverra auctor egestas. Nulla condimentum ac metus quam turpis gravida ut velit. Porta justo lacus consequat sed platea. Ut dui massa quam elit faucibus consectetur sapien aenean auctor. Felis ipsum amet justo in. Netus amet in egestas sed auctor lorem. Justo ullamcorper velit habitasse lorem eu arcu. Non enim a elit urna eget nibh quisque donec condimentum. Elit ut pellentesque neque in quis at viverra. Nisl etiam tristique odio eget convallis.',
               style: GoogleFonts.lato( 
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                color: Colors.white
+              ),
+              ),
+              SizedBox(
+                height: Get.height*0.04,
+              ),
+              Text('Lorem ipsum dolor sit amet consectetur. Ultrices id feugiat venenatis habitant mattis viverra elementum purus volutpat. Lacus eu molestie pulvinar rhoncus integer proin elementum. Pretium sit fringilla massa tristique aenean commodo leo. Aliquet viverra amet sit porta elementum et pellentesque posuere. Ullamcorper viverra tortor lobortis viverra auctor egestas. Nulla condimentum ac metus quam turpis gravida ut velit. Porta justo lacus consequat sed platea. Ut dui massa quam elit faucibus consectetur sapien aenean auctor. Felis ipsum amet justo in. Netus amet in egestas sed auctor lorem. Justo ullamcorper velit habitasse lorem eu arcu. Non enim a elit urna eget nibh quisque donec condimentum. Elit ut pellentesque neque in quis at viverra. Nisl etiam tristique odio eget convallis.',
+              style: GoogleFonts.roboto(
                 fontWeight: FontWeight.w400,
                 fontSize: 14,
                 color: Colors.white
