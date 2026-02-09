f34c7d082106733a02d3ae90c332054f5456d678
rayhantmt <rayhantmt@gmail.com>
2026-02-10 02:41:01 +0600

    added half text

--- lib/modules/contact_us/contact_us.dart
+++ lib/modules/contact_us/contact_us.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/utils/app_images.dart';
 
 class ContactUs extends StatelessWidget {
@@ -9,6 +11,48 @@ class ContactUs extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: Get.height*0.06,),
+              Row(
+                children: [
+                  Image.asset(AppImages.backicon,
+                  height: Get.height*0.06,
+                  ),
+                  SizedBox(width: Get.width*0.05,),
+                  Text('Contact Us',
+                  style: GoogleFonts.openSans( 
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    color: Colors.white
+                  ),
+                  )
+                ],
+              ),
+              SizedBox(height: Get.height*0.04,),
+              Text('Contact Us',
+              style:GoogleFonts.roboto(
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+                color: Colors.white
+              ) ,
+              ),
+
+              Text('Lorem ipsum dolor sit amet consectetur. Ultrices id feugiat venenatis habitant mattis viverra elementum purus volutpat. Lacus eu molestie pulvinar rhoncus integer proin elementum. Pretium sit fringilla massa tristique aenean commodo leo. Aliquet viverra amet sit porta elementum et pellentesque posuere. Ullamcorper viverra tortor lobortis viverra auctor egestas. Nulla condimentum ac metus quam turpis gravida ut velit. Porta justo lacus consequat sed platea. Ut dui massa quam elit faucibus consectetur sapien aenean auctor. Felis ipsum amet justo in. Netus amet in egestas sed auctor lorem. Justo ullamcorper velit habitasse lorem eu arcu. Non enim a elit urna eget nibh quisque donec condimentum. Elit ut pellentesque neque in quis at viverra. Nisl etiam tristique odio eget convallis.',
+              style: GoogleFonts.lato( 
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+                color: Colors.white
+              ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/modules/profile/profile_view.dart
+++ lib/modules/profile/profile_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/common_widgets/profile_container.dart';
