65cf8fa58cbd1fc8fded9fd4355682252f14a2db
rayhantmt <rayhantmt@gmail.com>
2026-02-09 23:40:50 +0600

    added the background colour

--- lib/modules/quote_summary/quote_summary_view.dart
+++ lib/modules/quote_summary/quote_summary_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class QuoteSummaryView extends StatelessWidget {
   const QuoteSummaryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: Get.height*0.07,)
+        ],
+      ),
+    );
   }
 }
