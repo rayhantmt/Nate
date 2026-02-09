7c3de41495d70f0db490781a45f59579ec329185
rayhantmt <rayhantmt@gmail.com>
2026-02-10 00:38:37 +0600

    formatted the document

--- lib/modules/quote_summary/quote_summary_view.dart
+++ lib/modules/quote_summary/quote_summary_view.dart
@@ -14,55 +14,55 @@ class QuoteSummaryView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            SizedBox(height: Get.height*0.07,),
+            SizedBox(height: Get.height * 0.07),
             Row(
               children: [
                 GestureDetector(
                   onTap: () => Get.back(),
-                  child: Image.asset(AppImages.backicon,
-                  height: Get.height*0.08,
+                  child: Image.asset(
+                    AppImages.backicon,
+                    height: Get.height * 0.08,
                   ),
                 ),
-                SizedBox(width: Get.width*0.1,),
-                Text('Quote Summary',
-                style: GoogleFonts.openSans( 
-                  color: Colors.white
-                ),
+                SizedBox(width: Get.width * 0.1),
+                Text(
+                  'Quote Summary',
+                  style: GoogleFonts.openSans(color: Colors.white),
                 ),
               ],
             ),
-            SizedBox(height: Get.height*0.05,),
+            SizedBox(height: Get.height * 0.05),
             Container(
-              height: Get.height*0.45,
+              height: Get.height * 0.45,
               width: double.infinity,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(12),
-                color: Color(0xff999999).withOpacity(0.3)
+                color: Color(0xff999999).withOpacity(0.3),
               ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Column(
                   children: [
-                    Image.asset(AppImages.homelogo,
-                    height: Get.height*0.1,
+                    Image.asset(AppImages.homelogo, height: Get.height * 0.1),
+                    SizedBox(height: Get.height * 0.05),
+                    Text(
+                      '120 Baker Street, Los Angeles, CA 90012',
+                      style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
                     ),
-                    SizedBox(height: Get.height*0.05,),
-                    Text('120 Baker Street, Los Angeles, CA 90012',
-                    style: GoogleFonts.roboto( 
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
-                      color: Colors.white
-                    ),
-                    ),
-                    SizedBox(height: Get.height*0.02,),
+                    SizedBox(height: Get.height * 0.02),
                     Row(
                       children: [
-                        Text('Order Details:',
-                        style: GoogleFonts.roboto( 
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16,
-                          color: Colors.white
-                        ),
+                        Text(
+                          'Order Details:',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
                         ),
                       ],
                     ),
@@ -72,14 +72,15 @@ class QuoteSummaryView extends StatelessWidget {
                       height: 1,
                       color: Colors.white.withOpacity(0.2),
                     ),
-                    Text('(1) 5" × 8" rectangle vents with frames, matte white finish',
-                    style: GoogleFonts.roboto( 
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14,
-                      color: Colors.white
+                    Text(
+                      '(1) 5" × 8" rectangle vents with frames, matte white finish',
+                      style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                        color: Colors.white,
+                      ),
                     ),
-                    ),
-                      Divider(
+                    Divider(
                       indent: 1,
                       endIndent: 1,
                       height: 1,
@@ -87,27 +88,29 @@ class QuoteSummaryView extends StatelessWidget {
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [ 
-                        Text('Total',
-                        style: GoogleFonts.roboto( 
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16,
-                          color: Colors.white
+                      children: [
+                        Text(
+                          'Total',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
                         ),
+                        Text(
+                          "\$78.54",
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
                         ),
-                        Text("\$78.54",
-                        style: GoogleFonts.roboto( 
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16,
-                          color: Colors.white
-                        ),
-                        )
                       ],
-                    )
+                    ),
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
