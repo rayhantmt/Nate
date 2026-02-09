1d2ded6141ddf7a5a27dae7613a74ff1e77fcba9
rayhantmt <rayhantmt@gmail.com>
2026-02-09 23:43:24 +0600

    added the background color

--- lib/modules/quote_summary/quote_summary_view.dart
+++ lib/modules/quote_summary/quote_summary_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nate/utils/app_images.dart';
 
 class QuoteSummaryView extends StatelessWidget {
   const QuoteSummaryView({super.key});
@@ -7,6 +8,7 @@ class QuoteSummaryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppImages.primarycolor,
       body: Column(
         children: [
           SizedBox(height: Get.height*0.07,)
