e2587c48e63e047ab772ed87d422f4825ce8a2cf
rayhantmt <rayhantmt@gmail.com>
2026-02-10 02:49:43 +0600

    added the app bar section

--- lib/modules/home_project_details/home_project_details_view.dart
+++ lib/modules/home_project_details/home_project_details_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/utils/app_images.dart';
 
 class HomeProjectDetailsView extends StatelessWidget {
@@ -11,7 +13,17 @@ class HomeProjectDetailsView extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.asset(AppImages.backicon)
+            Image.asset(AppImages.backicon,
+            height: Get.height*0.04,
+            ),
+            SizedBox(width: Get.width*0.04,),
+            Text('Project Details',
+            style: GoogleFonts.openSans(
+              fontWeight: FontWeight.w700,
+              fontSize: 20,
+              color: Colors.white
+            ),
+            )
           ],
         ),
       ),
