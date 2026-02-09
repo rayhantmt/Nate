210722ae7073f3be4abd649682a318eec5e88051
rayhantmt <rayhantmt@gmail.com>
2026-02-09 23:23:41 +0600

    added the quote summary section

---
+++ lib/modules/quote_summary/quote_summary_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuoteSummaryView extends StatelessWidget {
+  const QuoteSummaryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
