c7cfd9985a86ddbfc5adb985b5d6cb3fba08cce9
rayhantmt <rayhantmt@gmail.com>
2026-02-14 04:01:48 +0600

    added the app bar

--- lib/modules/shopping_cart/shopping_cart_view.dart
+++ lib/modules/shopping_cart/shopping_cart_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/utils/app_images.dart';
 
 class ShoppingCartView extends StatelessWidget {
@@ -8,6 +10,28 @@ class ShoppingCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
+      body: Column(
+        children: [
+          SizedBox(
+            height: Get.height*0.06,
+          ),
+          Row(
+            children: [
+              Image.asset(AppImages.backicon,
+              height: Get.height*0.06,
+              ),
+              SizedBox(width: Get.width*0.05,),
+              Text('Shopping Cart',
+              style: GoogleFonts.openSans( 
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+                color: Colors.white
+              ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
