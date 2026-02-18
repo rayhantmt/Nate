e1fe15b9df4136afba4e0f1859e8b0caa20b8b5b
rayhantmt <rayhantmt@gmail.com>
2026-02-19 00:12:08 +0600

    created the purchase history section

---
+++ lib/modules/purchase_history/purchase_history_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:nate/utils/app_images.dart';
+
+class PurchaseHistoryView extends StatelessWidget {
+  const PurchaseHistoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+    );
+  }
+}
