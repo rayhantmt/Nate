244df23e61793ab209d1d99de56bfb1c58b8e2a9
rayhantmt <rayhantmt@gmail.com>
2026-02-19 00:23:31 +0600

    added horizental padding

--- lib/modules/purchase_history/purchase_history_view.dart
+++ lib/modules/purchase_history/purchase_history_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:nate/utils/app_images.dart';
 
 class PurchaseHistoryView extends StatelessWidget {
@@ -8,6 +9,15 @@ class PurchaseHistoryView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: Get.height*0.07,),
+            
+          ],
+        ),
+      ),
     );
   }
 }
