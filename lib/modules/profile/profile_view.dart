309ed6dd60aa5317b506b1067c5c92026b32f0f2
rayhantmt <rayhantmt@gmail.com>
2026-02-19 01:12:55 +0600

    added the log out image and added singlechild scrollview

--- lib/modules/profile/profile_view.dart
+++ lib/modules/profile/profile_view.dart
@@ -21,102 +21,106 @@ class ProfileView extends StatelessWidget {
           ),
           Padding(
             padding: EdgeInsets.only(left: 20, right: 20),
-            child: Column(
-              children: [
-                SizedBox(height: Get.height * 0.05),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Profile',
-                      style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 32,
-                        color: Colors.white,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: Get.height * 0.05),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Profile',
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 32,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: () => Get.toNamed(AppPages.settings),
+                      GestureDetector(
+                        onTap: () => Get.toNamed(AppPages.settings),
+                        child: Image.asset(
+                          AppImages.settingsicon,
+                          height: Get.height * 0.05,
+                          width: Get.width * 0.1,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: Get.height * 0.02),
+                  CircleAvatar(
+                    radius: Get.height * 0.07,
+                    child: ClipOval(
                       child: Image.asset(
-                        AppImages.settingsicon,
-                        height: Get.height * 0.05,
-                        width: Get.width * 0.1,
+                        AppImages.profileimg,
+                        fit: BoxFit.cover,
+                        width: Get.height * 0.14,
+                        height: Get.height * 0.14,
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: Get.height * 0.02),
-                CircleAvatar(
-                  radius: Get.height * 0.07,
-                  child: ClipOval(
-                    child: Image.asset(
-                      AppImages.profileimg,
-                      fit: BoxFit.cover,
-                      width: Get.height * 0.14,
-                      height: Get.height * 0.14,
+                  ),
+                  SizedBox(height: Get.height * 0.01),
+                  Text(
+                    'Lukas Wanger',
+                    style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 24,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-                SizedBox(height: Get.height * 0.01),
-                Text(
-                  'Lukas Wanger',
-                  style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 24,
-                    color: Colors.white,
+                  SizedBox(height: Get.height * 0.02),
+                  GestureDetector(
+                    onTap: () => Get.toNamed(AppPages.editprofile),
+                    child: ProfileContainer(
+                      tittle: 'Edit Profile Details',
+                      image: AppImages.editprofile,
+                    ),
                   ),
-                ),
-                SizedBox(height: Get.height * 0.02),
-                GestureDetector(
-                  onTap: () => Get.toNamed(AppPages.editprofile),
-                  child: ProfileContainer(
-                    tittle: 'Edit Profile Details',
-                    image: AppImages.editprofile,
+                  SizedBox(height: Get.height * 0.015),
+                  GestureDetector(
+                    onTap: () => Get.toNamed(AppPages.savedprojects),
+                    child: ProfileContainer(
+                      tittle: 'Saved Projects',
+                      image: AppImages.savedporjects,
+                    ),
                   ),
-                ),
-                SizedBox(height: Get.height * 0.015),
-                GestureDetector(
-                  onTap: () => Get.toNamed(AppPages.savedprojects),
-                  child: ProfileContainer(
-                    tittle: 'Saved Projects',
-                    image: AppImages.savedporjects,
+                  SizedBox(height: Get.height * 0.015),
+                  GestureDetector(
+                    onTap: () => Get.toNamed(AppPages.shoppingcart),
+                    child: ProfileContainer(
+                      tittle: 'Shopping Cart',
+                      image: AppImages.savedporjects,
+                    ),
                   ),
-                ),
-                SizedBox(height: Get.height * 0.015),
-                GestureDetector(
-                  onTap: () => Get.toNamed(AppPages.shoppingcart),
-                  child: ProfileContainer(
-                    tittle: 'Shopping Cart',
-                    image: AppImages.savedporjects,
+                  SizedBox(height: Get.height * 0.015),
+                  GestureDetector(
+                    onTap: () => Get.toNamed(AppPages.purchasehistory),
+                    child: ProfileContainer(
+                      tittle: 'Purchase History',
+                      image: AppImages.historyicon,
+                    ),
                   ),
-                ),
-                SizedBox(height: Get.height * 0.015),
-                GestureDetector(
-                  onTap: () => Get.toNamed(AppPages.purchasehistory),
-                  child: ProfileContainer(
-                    tittle: 'Purchase History',
-                    image: AppImages.historyicon,
+                  SizedBox(height: Get.height * 0.015),
+                  GestureDetector(
+                    onTap: () => Get.toNamed(AppPages.contactus),
+                    child: ProfileContainer(
+                      tittle: 'Contact Us',
+                      image: AppImages.contacticon,
+                    ),
                   ),
-                ),
-                SizedBox(height: Get.height * 0.015),
-                GestureDetector(
-                  onTap: () => Get.toNamed(AppPages.contactus),
-                  child: ProfileContainer(
-                    tittle: 'Contact Us',
-                    image: AppImages.contacticon,
+                  SizedBox(height: Get.height * 0.015),
+                  ProfileContainer(
+                    tittle: 'Privacy Policy',
+                    image: AppImages.policy,
                   ),
-                ),
-                SizedBox(height: Get.height * 0.015),
-                ProfileContainer(
-                  tittle: 'Privacy Policy',
-                  image: AppImages.policy,
-                ),
-                SizedBox(height: Get.height * 0.015),
-                ProfileContainer(
-                  tittle: 'Terms & condition',
-                  image: AppImages.terms,
-                ),
-              ],
+                  SizedBox(height: Get.height * 0.015),
+                  ProfileContainer(
+                    tittle: 'Terms & condition',
+                    image: AppImages.terms,
+                  ),
+                  //SizedBox(height: Get.height*0.05,),
+                  Image.asset(AppImages.logout)
+                ],
+              ),
             ),
           ),
         ],
