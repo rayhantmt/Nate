92f15132861885f6b774663c50d23f68a7d4c56a
rayhantmt <rayhantmt@gmail.com>
2026-02-19 00:06:45 +0600

    done with the quote summary page

--- lib/modules/quote_summary/quote_summary_view.dart
+++ lib/modules/quote_summary/quote_summary_view.dart
@@ -14,134 +14,141 @@ class QuoteSummaryView extends StatelessWidget {
       backgroundColor: AppImages.primarycolor,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(height: Get.height * 0.07),
-            Row(
-              children: [
-                GestureDetector(
-                  onTap: () => Get.back(),
-                  child: Image.asset(
-                    AppImages.backicon,
-                    height: Get.height * 0.08,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: Get.height * 0.07),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => Get.back(),
+                    child: Image.asset(
+                      AppImages.backicon,
+                      height: Get.height * 0.08,
+                    ),
+                  ),
+                  SizedBox(width: Get.width * 0.1),
+                  Text(
+                    'Quote Summary',
+                    style: GoogleFonts.openSans(color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(height: Get.height * 0.05),
+              Container(
+                height: Get.height * 0.45,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Color(0xff999999).withOpacity(0.3),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      Image.asset(AppImages.homelogo, height: Get.height * 0.1),
+                      SizedBox(height: Get.height * 0.05),
+                      Text(
+                        '120 Baker Street, Los Angeles, CA 90012',
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: Get.height * 0.02),
+                      Row(
+                        children: [
+                          Text(
+                            'Order Details:',
+                            style: GoogleFonts.roboto(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        indent: 1,
+                        endIndent: 1,
+                        height: 1,
+                        color: Colors.white.withOpacity(0.2),
+                      ),
+                      Text(
+                        '(1) 5" × 8" rectangle vents with frames, matte white finish',
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Divider(
+                        indent: 1,
+                        endIndent: 1,
+                        height: 1,
+                        color: Colors.white.withOpacity(0.2),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Total',
+                            style: GoogleFonts.roboto(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "\$78.54",
+                            style: GoogleFonts.roboto(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(width: Get.width * 0.1),
-                Text(
-                  'Quote Summary',
-                  style: GoogleFonts.openSans(color: Colors.white),
-                ),
-              ],
-            ),
-            SizedBox(height: Get.height * 0.05),
-            Container(
-              height: Get.height * 0.45,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Color(0xff999999).withOpacity(0.3),
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
+              SizedBox(height: Get.height * 0.11),
+              GestureDetector(
+                onTap: () => Get.toNamed(AppPages.afterquotesummary),
+                child: CommonButton(tittle: 'Add to shopping cart'),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              GestureDetector(
+                onTap: () => Get.toNamed(AppPages.savedprojects),
+                child: CommonButton(tittle: 'Add to saved projects'),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Container(
+                height: Get.height * 0.05,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Color(0xff2E3030)),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset(AppImages.homelogo, height: Get.height * 0.1),
-                    SizedBox(height: Get.height * 0.05),
+                    Image.asset(AppImages.downloadlog),
                     Text(
-                      '120 Baker Street, Los Angeles, CA 90012',
+                      'Get PDF Receipt',
                       style: GoogleFonts.roboto(
                         fontWeight: FontWeight.w600,
                         fontSize: 16,
-                        color: Colors.white,
+                        color: Color(0xffEC1B34),
                       ),
-                    ),
-                    SizedBox(height: Get.height * 0.02),
-                    Row(
-                      children: [
-                        Text(
-                          'Order Details:',
-                          style: GoogleFonts.roboto(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      indent: 1,
-                      endIndent: 1,
-                      height: 1,
-                      color: Colors.white.withOpacity(0.2),
-                    ),
-                    Text(
-                      '(1) 5" × 8" rectangle vents with frames, matte white finish',
-                      style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Divider(
-                      indent: 1,
-                      endIndent: 1,
-                      height: 1,
-                      color: Colors.white.withOpacity(0.2),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Total',
-                          style: GoogleFonts.roboto(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          "\$78.54",
-                          style: GoogleFonts.roboto(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
                     ),
                   ],
                 ),
               ),
-            ),
-            SizedBox(height: Get.height * 0.2),
-            GestureDetector(
-              onTap: () => Get.toNamed(AppPages.afterquotesummary),
-              child: CommonButton(tittle: 'Add to shopping cart'),
-            ),
-            SizedBox(height: Get.height * 0.02),
-            Container(
-              height: Get.height * 0.05,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Color(0xff2E3030)),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(AppImages.downloadlog),
-                  Text(
-                    'Get PDF Receipt',
-                    style: GoogleFonts.roboto(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
-                      color: Color(0xffEC1B34),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
