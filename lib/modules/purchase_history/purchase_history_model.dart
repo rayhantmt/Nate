fa36ea2f638036b2865f4dd62146262890abf25b
rayhantmt <rayhantmt@gmail.com>
2026-02-19 00:17:29 +0600

    created the data model for purchase history

---
+++ lib/modules/purchase_history/purchase_history_model.dart
@@ -0,0 +1,9 @@
+class PurchaseHistoryModel {
+  final String tittle;
+  final String price;
+  final String vents;
+  final bool completed;
+
+  PurchaseHistoryModel({required this.tittle, required this.price, required this.vents, required this.completed});
+  
+}
