424ddf014317dd44ceac9abc49d45625c64f52fd
rayhantmt <rayhantmt@gmail.com>
2026-02-19 00:37:38 +0600

    added the listview

--- lib/modules/purchase_history/purchase_history_view.dart
+++ lib/modules/purchase_history/purchase_history_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nate/modules/purchase_history/purchase_history_controller.dart';
 import 'package:nate/utils/app_images.dart';
 
-class PurchaseHistoryView extends StatelessWidget {
+class PurchaseHistoryView extends GetView<PurchaseHistoryController> {
   const PurchaseHistoryView({super.key});
 
   @override
@@ -14,30 +15,38 @@ class PurchaseHistoryView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            SizedBox(height: Get.height*0.07,),
+            SizedBox(height: Get.height * 0.07),
             Row(
               children: [
-                Image.asset(AppImages.backicon,
-                height: Get.height*0.06,
+                Image.asset(AppImages.backicon, height: Get.height * 0.06),
+                SizedBox(width: Get.width * 0.05),
+                Text(
+                  'History',
+                  style: GoogleFonts.openSans(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
-                SizedBox(width: Get.width*0.05,),
-                Text('History',
-                style: GoogleFonts.openSans( 
-                  fontWeight: FontWeight.w700,
-                  fontSize: 20,
-                  color: Colors.white
-                ),
-                )
               ],
             ),
-            SizedBox(height: Get.height*0.04,),
-            ListView.builder(itemBuilder: (context, index) => Container(
-              height: Get.height*0.15,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18),
-                color: Colors.white.withOpacity(0.05)
+            SizedBox(height: Get.height * 0.04),
+            Expanded(
+              child: ListView.builder(
+                itemCount: controller.purchasehistroy.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Container(
+                    height: Get.height * 0.15,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Colors.white.withOpacity(0.05),
+                    ),
+                    child: Text(controller.purchasehistroy[index].tittle),
+                  ),
+                ),
               ),
-            ),)
+            ),
           ],
         ),
       ),
