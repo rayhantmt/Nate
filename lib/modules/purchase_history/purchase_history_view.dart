d9c6b6c192a20c876341f11256d9d7218bdde2da
rayhantmt <rayhantmt@gmail.com>
2026-02-19 00:30:07 +0600

    adding the listview

--- lib/modules/purchase_history/purchase_history_view.dart
+++ lib/modules/purchase_history/purchase_history_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:nate/utils/app_images.dart';
 
 class PurchaseHistoryView extends StatelessWidget {
@@ -18,9 +19,25 @@ class PurchaseHistoryView extends StatelessWidget {
               children: [
                 Image.asset(AppImages.backicon,
                 height: Get.height*0.06,
+                ),
+                SizedBox(width: Get.width*0.05,),
+                Text('History',
+                style: GoogleFonts.openSans( 
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                  color: Colors.white
+                ),
                 )
               ],
-            )
+            ),
+            SizedBox(height: Get.height*0.04,),
+            ListView.builder(itemBuilder: (context, index) => Container(
+              height: Get.height*0.15,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                color: Colors.white.withOpacity(0.05)
+              ),
+            ),)
           ],
         ),
       ),
